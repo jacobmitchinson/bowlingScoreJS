dbe9dccae7c89d35c1b8d23281a208b080e14803
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-24 14:13:53 +0000

    Added ability to record score for each roll and tested.

--- js/frame.js
+++ js/frame.js
@@ -2,14 +2,23 @@ var Frame = function() {
 
   this.rollCount = 0;
   this.isOver = false;
+  this.score = 0;
 
 };
 
-Frame.prototype.roll = function() {
+Frame.prototype.roll = function(hits) {
 
  var roll = new Roll();
+ roll.inputForPinHits(hits);
+ this.recordHits(roll);
  this.rollCount += 1;
  this.checkOver();
+
+};
+
+Frame.prototype.recordHits = function(roll) {
+
+  this.score += roll.totalNumberOfHitPins;
 
 };
 
--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -2,19 +2,25 @@ describe("Frame", function() {
 
   var frame;
 
-
+  beforeEach( function() {
+    frame = new Frame();
+  });
 
   it("should know how many rolls there have been", function() {
-    frame = new Frame();
+
     frame.roll();
     expect(frame.rollCount).toEqual(1);
   });
 
   it("should know that there are a maximum of 2 rolls", function() {
-    frame = new Frame();
     frame.roll();
     frame.roll();
     expect(frame.isOver).toBe(true);
+  });
+
+  it("should record the score from each roll", function() {
+    frame.roll(1);
+    expect(frame.score).toEqual(1);
   });
 
 

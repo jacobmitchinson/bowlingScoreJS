020a983efda72ae2060c94567c108fe6181ac0af
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-24 16:39:30 +0000

    roll knows when it is a strike

--- js/roll.js
+++ js/roll.js
@@ -10,3 +10,11 @@ Roll.prototype.inputForPinHits = function(hits) {
   this.totalNumberOfHitPins += hits;
 
 };
+
+Roll.prototype.isStrike = function() {
+
+  if (this.totalNumberOfHitPins === 10) {
+    return true;
+  }
+
+};
